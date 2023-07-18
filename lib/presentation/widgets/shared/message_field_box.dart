533bc7b41add5ffa879372d428e411c429cd4bdc
Axely7 <axely7@hotmail.com>
2023-07-17 19:07:43 -0600

    not showing keyboard and creating business logic

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,18 +5,39 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
+    final outlineInputBorder = UnderlineInputBorder(
+        borderSide: const BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(40));
+
+    final inputDecoration = InputDecoration(
+        hintText: 'End your message with a "?"',
+        enabledBorder: outlineInputBorder,
+        focusedBorder: outlineInputBorder,
+        filled: true,
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.send_outlined),
+          onPressed: () {
+            final textValue = textController.value.text;
+            print('button: $textValue');
+            textController.clear();
+          },
+        ));
 
     return TextFormField(
-      decoration: InputDecoration(
-          enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: colors.primary),
-              borderRadius: BorderRadius.circular(20)),
-          filled: true,
-          suffixIcon: IconButton(
-            icon: const Icon(Icons.send_outlined),
-            onPressed: () {},
-          )),
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print('Submit $value');
+        textController.clear();
+        focusNode.requestFocus();
+      },
     );
   }
 }
