ab0d44e7b98cb28d987ba099042b7d7843e99f91
Axely7 <axely7@hotmail.com>
2023-07-19 09:40:04 -0600

    making a http request

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
+
   List<Message> messageList = [
     Message(text: 'Hola ', fromWho: FromWho.mine),
     Message(text: 'Tienes algún invento nuevo?', fromWho: FromWho.mine)
   ];
 
   Future<void> sendMessage(String text) async {
-    //TODO: Implementar método
+    if (text.isEmpty) return;
+
+    final newMessage = Message(text: text, fromWho: FromWho.mine);
+
+    messageList.add(newMessage);
+
+    if (text.endsWith('?')) {
+      await hisReply();
+    }
+
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> hisReply() async {
+    final hisMessage = await getYesNoAnswer.getAnswer();
+  }
+
+  Future<void> moveScrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    chatScrollController.animateTo(
+        chatScrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut);
   }
 }
--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -39,6 +39,7 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
               child: ListView.builder(
+                controller: chatProvider.chatScrollController,
                 itemCount: chatProvider.messageList.length,
                 itemBuilder: (context, index) {
                   final message = chatProvider.messageList[index];
@@ -51,7 +52,9 @@ class _ChatView extends StatelessWidget {
                 },
               ),
             ),
-            const MessageFieldBox(),
+            MessageFieldBox(
+              onValue: chatProvider.sendMessage,
+            ),
           ],
         ),
       ),
